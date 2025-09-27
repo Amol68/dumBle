73e5b0752a8b089d2246e2a4ef3ccf0fd2b8697b
AmolFyntune <kadam.amol@fyntune.com>
2025-09-27 14:24:01 +0530

    feature:api level data sanitization

--- src/app.js
+++ src/app.js
@@ -15,16 +15,88 @@ connectDB()
     console.log("Database Connection Failed");
   });
 
-app.use(express.json());
+app.use(express.json()); // global mw to convert JSON into object
 
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
   const user = new User(req.body);
   try {
     await user.save();
     res.send("User added successfully");
   } catch (error) {
     res.status(400).send(`Error Saving The User ${error.message}`);
+  }
+});
+
+// feed API: get all users from DB
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    if (users.length === 0) {
+      res.status(404).send("Users not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+// feed API: get a single user from DB by email
+app.get("/user", async (req, res) => {
+  const userLName = req.body.lastName;
+
+  try {
+    const user = await User.findOne({ lastName: userLName });
+
+    if (user.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("User not found");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const id = req.body.id;
+
+  try {
+    const user = await User.findByIdAndDelete(id);
+    res.send("User deleted successfully");
+  } catch {
+    res.status(400).send("User cannot be deleted");
+  }
+});
+
+app.patch("/user/:userID", async (req, res) => {
+  const body = req.body;
+  const userID = req.params?.userID;
+
+  const allowedFields = [
+    "id",
+    "firstName",
+    "lastName",
+    "password",
+    "about",
+    "gender",
+    "skills",
+  ];
+
+  const isUpdateAllowed = Object.keys(body).every((key) =>
+    allowedFields.includes(key)
+  );
+  if (!isUpdateAllowed) res.send("Update Not Allowed");
+
+  if (req.body.skills > 10) res.send("Too Many Skills");
+
+  try {
+    await User.findByIdAndUpdate({ _id: userID }, body, {
+      runValidators: true,
+    });
+    res.send("User Updated Successfully");
+  } catch (err) {
+    res.status(500).send("User Update Failed:" + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,53 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [8, "Minimum 6 characters"],
+      maxLength: [15, "Maximum 15 characters"],
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(val) {
+        if (!["Male", "Female", "Other"].includes(val)) {
+          throw new Error("Gender not allowed");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is about me",
+    },
+    photoUrl: {
+      type: String,
+      default: "www.dummyimage.com",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const userModel = mongoose.model("User", userSchema);
 
